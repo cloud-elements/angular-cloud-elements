f950350bb94ede3f9bba8c7bab4732a2fdc97926
Brad Scharmann <bscharm@gmail.com>
2016-01-22 15:55:46 -0700

    update CloudElements provider to allow setting options from outside config block

--- dist/angular-cloud-elements.js
+++ dist/angular-cloud-elements.js
@@ -26,41 +26,46 @@
 
 })(angular);
 
-angular
-  .module('angularCloudElements')
-  .provider('CloudElements', CloudElements);
+(function () {
+  angular
+    .module('angularCloudElements')
+    .provider('CloudElements', CloudElements);
 
-function CE(options) {
-  this.options = options;
-  this.getUserSecret = function() {
-    return this.options.userSecret;
-  }
-  this.getBaseUrl = function() {
-    return this.options.baseUrl;
-  }
-}
-
-function CloudElements() {
-
-  this.options = {
-    orgSecret: '',
-    userSecret: '',
-    baseUrl: 'http://localhost:8080/elements/api-v2'
+  function CE(options) {
+    this.options = options;
+    this.setOptions = setOptions;
+    this.getUserSecret = function () {
+      return this.options.userSecret;
+    }
+    this.getBaseUrl = function () {
+      return this.options.baseUrl;
+    }
   };
 
-  this.setOptions = function (options) {
+  function CloudElements() {
+
+    this.options = {
+      orgSecret: '',
+      userSecret: '',
+      baseUrl: 'http://localhost:8080/elements/api-v2'
+    };
+
+    this.setOptions = setOptions;
+
+    this.$get = [function () {
+        return new CE(this.options);
+      }
+    ];
+
+  }
+
+  function setOptions(options) {
     if (!angular.isObject(options)) {
       throw new Error("Options must be an object");
     }
     this.options = options;
   };
-
-  this.$get = [function () {
-      return new CE(this.options);
-    }
-  ];
-
-}
+})();
 
 (function () {
   'use strict';
