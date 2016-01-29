07db376ca367f6fd9596439748fce75382c3f3a7
Brad Scharmann <bscharm@gmail.com>
2016-01-29 15:29:13 -0700

    flesh out ceFormulas service

---
+++ src/angular-cloud-elements/services/ceFormulas.service.js
@@ -0,0 +1,68 @@
+(function () {
+  'use strict';
+
+  angular
+    .module('angularCloudElements.services')
+    .factory('ceFormulas', ceFormulas);
+
+  ceFormulas.$inject = ['httpUtility', 'ceAuth'];
+
+  function ceFormulas(httpUtility, ceAuth) {
+
+    ceAuth.validateConfig();
+
+    return {
+      getFormulas: getFormulas,
+      getFormula: getFormula,
+      getFormulaInstances: getFormulaInstances,
+      getAllFormulaInstances: getAllFormulaInstances,
+      createFormulaInstance: createFormulaInstance,
+      updateFormulaInstance: updateFormulaInstance,
+      deleteFormulaInstance: deleteFormulaInstance,
+      getAllFormulaInstanceExecutions: getAllFormulaInstanceExecutions,
+      getFormulaInstanceExecutions: getFormulaInstanceExecutions
+    };
+
+    function getFormulas() {
+      return httpUtility.get('/formulas');
+    }
+
+    function getFormula(formulaId) {
+      return httpUtility.get('/formulas/' + formulaId);
+    }
+
+    function getFormulaInstances(formulaId) {
+      return httpUtility.get('/formulas/' + formulaId + '/instances');
+    }
+
+    function getAllFormulaInstances() {
+      return httpUtility.get('/formulas/instances');
+    }
+
+    function getFormulaInstance(formulaId, formulaInstanceId) {
+      return httpUtility.get('/formulas/' + formulaId + '/instances/' + formulaInstanceId);
+    }
+
+    function createFormulaInstance(formulaId, formulaInstance) {
+      return httpUtility.post('/formulas/' + formulaId + '/instances', formulaInstance);
+    }
+
+    function updateFormulaInstance(formulaId, formulaInstanceId, formulaInstance) {
+      return httpUtility.patch('/formulas/' + formulaId + '/instances/' + formulaInstanceId, formulaInstance);
+    }
+
+    function deleteFormulaInstance(formulaId, formulaInstanceId) {
+      return httpUtility.delete('/formulas/' + formulaId + '/instances/' + formulaInstanceId);
+    }
+
+    function getAllFormulaInstanceExecutions() {
+      return httpUtility.get('/formulas/instances/executions');
+    }
+
+    function getFormulaInstanceExecutions(formulaId, formulaInstanceId) {
+      return httpUtility.get('/formulas' + formulaId + '/instances/' + formulaInstanceId + '/executions');
+    }
+
+  }
+
+})();
